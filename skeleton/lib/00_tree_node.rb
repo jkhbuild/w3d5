7cf6db46600f2a1d324c08a1c9800084296e186f
jkhbuild <jkh.build@gmail.com>
2022-09-30 13:34:58 -0400

    parent= method started

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,21 @@
 class PolyTreeNode
 
+    attr_reader :parent, :children, :value
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent=(new_value=nil)
+        
+        @parent = new_value
+        if new_value.nil?
+            return nil
+        else !new_value.children.include?(self)
+            new_value.children << self
+        end
+    end
+
+
 end
